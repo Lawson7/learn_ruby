09d7a670e56200bb4583a968e53f0d07da4f6082
Harry Lawson <harryglawson@gmail.com>
2019-06-30 20:45:09 +1200

    3 methods and test creation complete

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,7 @@
-#write your code here
+def ftoc far
+    (far - 32) * 5/9r
+end
+
+def ctof cel
+    (cel * 1.8) + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,49 @@
-#write your code here
+def add a, b
+    a + b
+end
+
+def subtract a, b
+    a - b
+end
+
+def sum arr
+    result = 0
+    arr.each do |num|
+        result += num
+    end
+    result
+end
+
+def multiply (*arr)
+    result = 1
+    arr.each do |num|
+        result = result * num
+    end
+    result
+end
+
+def power a, b
+    a ** b
+end
+
+
+
+def factorial num
+    result = 1
+    arr = []
+
+    if num == 0
+        result = 1
+
+    else
+        while num > 0
+        arr.push(num)
+        num = num - 1
+        end
+
+        arr.each do |i|
+            result = result * i
+        end
+        result
+    end
+end
